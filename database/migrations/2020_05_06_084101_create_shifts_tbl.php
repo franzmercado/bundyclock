359d697b738ef8a5b30780819ee2033aad5b8677
Franz Mercado <franzmercadof@gmail.com>
2020-05-06 16:49:22 +0800

    tables added

---
+++ database/migrations/2020_05_06_084101_create_shifts_tbl.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShiftsTbl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->string('shift_ID');
+            $table->dateTime('time_in');
+            $table->dateTime('time_out');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shifts');
+    }
+}
